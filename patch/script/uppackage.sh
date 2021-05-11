4b239ce93286cd2980f96cf0c66f6f292e99932e
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-05-12 04:04:36 +0800

    autocore

--- patch/script/uppackage.sh
+++ patch/script/uppackage.sh
@@ -31,6 +31,7 @@ rm -rf ./package/lean/luci-app-netdata && svn co https://github.com/sirpdboy/sir
 rm -rf ./feeds/packages/admin/netdata && svn co https://github.com/sirpdboy/sirpdboy-package/trunk/netdata ./feeds/packages/admin/netdata
 rm -rf ./feeds/packages/net/https-dns-proxy  && svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy ./feeds/packages/net/https-dns-proxy
 #rm -rf ./package/diy/autocore
+rm -rf ./package/build/set/autocore
 #rm -rf ./package/diy/default-settings
 rm -rf ./feeds/packages/devel/ninja   && svn co https://github.com/Lienol/openwrt-packages/trunk/devel/ninja feeds/packages/devel/ninja
 rm -rf package/lean/luci-app-wrtbwmon
