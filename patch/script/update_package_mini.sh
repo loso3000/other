093bd8ae24a9a06b97b6def7e5c8ca443f236ffd
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-05-12 04:04:19 +0800

    autocore

--- patch/script/update_package_mini.sh
+++ patch/script/update_package_mini.sh
@@ -23,6 +23,7 @@ sed -i '60s/ITdesk01/sirpdboy/' ./package/jd_openwrt_script/files/jd_openwrt_scr
 rm -rf ./feeds/packages/devel/ninja   && svn co https://github.com/Lienol/openwrt-packages/trunk/devel/ninja feeds/packages/devel/ninja
 rm -rf package/lean/luci-app-wrtbwmon
 #rm -rf ./package/diy/autocore
+rm -rf ./package/build/set/autocore
 rm -rf ./package/diy/netdata
 rm -rf ./package/build/mwan3
 #rm -rf ./package/diy/default-settings
