9eb17ba343707f555d5a3aeaed4d60f64f13152b
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-06-05 17:11:18 +0800

    topd

--- patch/script/update_package_mini.sh
+++ patch/script/update_package_mini.sh
@@ -184,6 +184,8 @@ sed -i 's,default n,default y,g' package/lean/luci-app-vssr/Makefile
 #sed -i "/mediaurlbase/d" package/*/luci-theme*/root/etc/uci-defaults/*
 #sed -i "/mediaurlbase/d" feed/*/luci-theme*/root/etc/uci-defaults/*
 
+sed -i "s/bootstrap/opentopd/g" feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i 's/bootstrap/opentopd/g' feeds/luci/collections/luci/Makefile
 
 # R8168驱动
 # svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/kernel/r8152 package/new/r8152
