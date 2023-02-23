406671efa1700f1acea059585a6ba2d5a7f17651
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-02-23 13:32:19 +0800

    a

---
+++ up/luci-app-pdadplus/luasrc/controller/advancedplus.lua
@@ -0,0 +1,12 @@
+module("luci.controller.advancedplus",package.seeall)
+function index()
+	if not nixio.fs.access("/etc/config/advancedplus") then return end
+	if nixio.fs.access('/www/luci-static/kucat/css/style.css') then
+	    entry({"admin","system","advancedplus"},alias("admin","system","advancedplus","kucatset"),_("Advanced plus"),60)dependent = true
+	    entry({"admin","system","advancedplus","kucatset"},cbi("advancedplus/kucatset"),_("KuCat Theme Config"),10).leaf = true
+	    entry({"admin", "system","advancedplus","kucatupload"}, form("advancedplus/kucatupload"), _("Theme Background upload"), 20).leaf = true
+	else
+	   entry({"admin","system","advancedplus"},alias("admin","system","advancedplus","advancededit"),_("Advanced plus"),60)dependent = true
+	end
+	entry({"admin","system","advancedplus","advancededit"},cbi("advancedplus/advancededit"),_("Advanced Edit"),60).leaf = true
+end
