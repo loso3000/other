f556234d6c10ad870c127b7dbb0561ca5b128073
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-03-09 13:19:55 +0800

    -1

--- up/luci-app-owizard/luasrc/controller/netwizard.lua
+++ up/luci-app-owizard/luasrc/controller/netwizard.lua
@@ -6,6 +6,6 @@ function index()
 	if not nixio.fs.access("/etc/config/netwizard") then
 		return
 	end
-	entry({"admin","system", "netwizard"}, cbi("netwizard/netwizard"), _("Inital Setup"), 1).dependent = true
+	entry({"admin","system", "netwizard"}, cbi("netwizard/netwizard"), _("Inital Setup"), -1).dependent = true
 
 end
