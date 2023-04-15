b24e5404de8920ce8fdff3a9d5f739663efcbe2f
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-04-15 21:19:50 +0800

    net

--- up/luci-app-owizard/luasrc/controller/netwizard.lua
+++ up/luci-app-owizard/luasrc/controller/netwizard.lua
@@ -16,17 +16,3 @@ function index()
 	end
 	entry({"admin","system", "netwizard","settings"}, cbi("netwizard/netwizard"), _("Inital Setup"), 1).dependent = true
 end
-
-function hide_menu()
-	luci.sys.call("touch /etc/config/netwizard_hide")
-	luci.sys.call("rm -rf /tmp/luci-*")
-	-- luci.sys.call("/etc/init.d/rpcd restart >/dev/null")
-	luci.http.redirect(luci.dispatcher.build_url("admin", "system", "system"))
-end
-
-function show_menu()
-	luci.sys.call("rm -rf /etc/config/netwizard_hide")
-	luci.sys.call("rm -rf /tmp/luci-*")
-	-- luci.sys.call("/etc/init.d/rpcd restart >/dev/null")
-	luci.http.redirect(luci.dispatcher.build_url("admin","system", "netwizard","settings"))
-end
