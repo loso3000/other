1b2f6df07970df70af6bbb94ad26b313e1d03a7a
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-04-15 21:29:30 +0800

    n

--- up/netspeedtest/luci-app-netspeedtest/luasrc/model/cbi/netspeedtest/homebox.lua
+++ up/netspeedtest/luci-app-netspeedtest/luasrc/model/cbi/netspeedtest/homebox.lua
@@ -1,12 +1,14 @@
-local o = require "luci.sys"
-
+-- Copyright (C) 2020-2022  sirpdboy  <herboy2008@gmail.com> https://github.com/sirpdboy/netspeedtest
 
 local m, s ,o
 
+
 m = Map("netspeedtest", "<font color='green'>" .. translate("Net Speedtest") .."</font>",translate( "Network speed diagnosis test (including intranet and extranet)<br/>For specific usage, see:") ..translate("<a href=\'https://github.com/sirpdboy/netspeedtest.git' target=\'_blank\'>GitHub @sirpdboy/netspeedtest</a>") )
+-- m:section(SimpleSection).template  = "netspeedtest/homebox_status"
 
 s = m:section(TypedSection, "homebox", translate('Lan homebox Web'))
-s.anonymous = true
+s.addremove=false
+s.anonymous=true
 
 o=s:option(Flag,"enabled",translate("Enable"))
 o.default=0
@@ -15,8 +17,9 @@ o = s:option(DummyValue, '', '')
 o.rawhtml = true
 o.template ='netspeedtest/homebox'
 
-local o=luci.http.formvalue("cbi.apply")
-if o then
-  io.popen("/etc/init.d/netspeedtest restart")
+m.apply_on_parse = true
+m.on_after_apply = function(self,map)
+  io.popen("/etc/init.d/netspeedtest start")
 end
+
 return m
