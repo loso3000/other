89e7689c183ef3ad9f87b4a693bd75b8bd178ec6
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-06-09 20:06:14 +0800

    u

--- up/luci-app-netspeedlimit/luasrc/controller/netspeedlimit.lua
+++ up/luci-app-netspeedlimit/luasrc/controller/netspeedlimit.lua
@@ -2,8 +2,16 @@ module("luci.controller.netspeedlimit", package.seeall)
 
 function index()
 	if not nixio.fs.access("/etc/config/netspeedlimit") then return end
-	local page
-        entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
-	page = entry({"admin","control","netspeedlimit"},cbi("netspeedlimit"),_("Network speed limit"),67)
-	page.dependent = true
+	
+    entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
+    entry({"admin", "control", "netspeedlimit"}, cbi("netspeedlimit"), _("Netspeedlimit"), 10).dependent =true
+    entry({"admin", "control", "netspeedlimit", "status"}, call("act_status")).leaf = true
+
  end
+function act_status()
+    local sys  = require "luci.sys"
+    local e = {} 
+    e.status = sys.call("[ `(tc qdisc show dev br-lan | head -1) 2>/dev/null | grep -c 'default' 2>/dev/null` -gt 0 ] > /dev/null") == 0 
+    luci.http.prepare_content("application/json")
+    luci.http.write_json(e)
+end
--- up/luci-app-netspeedlimit/luasrc/model/cbi/netspeedlimit.lua
+++ up/luci-app-netspeedlimit/luasrc/model/cbi/netspeedlimit.lua
@@ -22,7 +22,7 @@ t.sortable  = true
 e = t:option(Flag, "enable", translate("Enable"))
 e.rmempty = false
 
-usr = t:option(Value, "usr",translate("List of Speed Limiting Machines"))
+usr = t:option(Value, "usr", translate("List of Speed Limiting Machines"))
 sys.net.mac_hints(function(mac, name)
 	usr:value(mac, "%s (%s)" %{ mac, name })
 end)
@@ -31,12 +31,11 @@ dl = t:option(Value, "download", translate("Downloads"))
 dl.rmempty = false
 dl.default = '0.002'
 dl.size = 6
-dl.datatype = "and(float,min(0.1))"
+
 ul = t:option(Value, "upload", translate("Uploads"))
 ul.rmempty = false
 ul.default = '0.002'
 ul.size = 6
-ul.datatype = "and(float,min(0.1))"
     function validate_time(self, value, section)
         local hh, mm, ss
         hh, mm, ss = string.match (value, "^(%d?%d):(%d%d)$")
