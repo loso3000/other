f7c02ca0fbfae8b8134e38dca36fc1a6f61b58fb
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-06-22 02:39:30 +0800

    m

--- up/mwan3/files/lib/mwan3/mwan3.sh
+++ up/mwan3/files/lib/mwan3/mwan3.sh
@@ -136,7 +136,7 @@ mwan3_set_custom_ipset()
 	mwan3_push_update flush mwan3_custom_ipv4
 	config_list_foreach "globals" "rt_table_lookup" mwan3_set_custom_ipset_v4
 
-	if [ $NO_IPV6 -eq 0 ]; then
+	if [ "$NO_IPV6" -eq 0 ]; then
 		mwan3_push_update -! create mwan3_custom_ipv6 hash:net family inet6
 		mwan3_push_update flush mwan3_custom_ipv6
 		config_list_foreach "globals" "rt_table_lookup" mwan3_set_custom_ipset_v6
@@ -188,7 +188,7 @@ mwan3_set_connected_ipv6()
 {
 	local connected_network_v6 error
 	local update=""
-	[ $NO_IPV6 -eq 0 ] || return
+	[ "$NO_IPV6" -eq 0 ] || return
 
 	mwan3_push_update -! create mwan3_connected_ipv6 hash:net family inet6
 	mwan3_push_update flush mwan3_connected_ipv6
@@ -209,7 +209,7 @@ mwan3_set_connected_ipset()
 	mwan3_push_update -! create mwan3_connected_ipv4 hash:net
 	mwan3_push_update flush mwan3_connected_ipv4
 
-	if [ $NO_IPV6 -eq 0 ]; then
+	if [ "$NO_IPV6" -eq 0 ]; then
 		mwan3_push_update -! create mwan3_connected_ipv6 hash:net family inet6
 		mwan3_push_update flush mwan3_connected_ipv6
 	fi
@@ -226,7 +226,7 @@ mwan3_set_dynamic_ipset()
 	mwan3_push_update -! create mwan3_dynamic_ipv4 list:set
 	mwan3_push_update flush mwan3_dynamic_ipv4
 
-	if [ $NO_IPV6 -eq 0 ]; then
+	if [ "$NO_IPV6" -eq 0 ]; then
 		mwan3_push_update -! create mwan3_dynamic_ipv6 hash:net family inet6
 		mwan3_push_update flush mwan3_dynamic_ipv6
 	fi
@@ -240,7 +240,7 @@ mwan3_set_general_rules()
 	local IP
 
 	for IP in "$IP4" "$IP6"; do
-		[ "$IP" = "$IP6" ] && [ $NO_IPV6 -ne 0 ] && continue
+		[ "$IP" = "$IP6" ] && [ "$NO_IPV6" -ne 0 ] && continue
 		RULE_NO=$((MM_BLACKHOLE+2000))
 		if [ -z "$($IP rule list | awk -v var="$RULE_NO:" '$1 == var')" ]; then
 			$IP rule add pref $RULE_NO fwmark $MMX_BLACKHOLE/$MMX_MASK blackhole
@@ -258,7 +258,7 @@ mwan3_set_general_iptables()
 	local IPT current update error family
 
 	for IPT in "$IPT4" "$IPT6"; do
-		[ "$IPT" = "$IPT6" ] && [ $NO_IPV6 -ne 0 ] && continue
+		[ "$IPT" = "$IPT6" ] && [ "$NO_IPV6" -ne 0 ] && continue
 		current="$($IPT -S)"$'\n'
 		update="*mangle"
 		if [ -n "${current##*-N mwan3_ifaces_in*}" ]; then
@@ -368,7 +368,7 @@ mwan3_create_iface_iptables()
 	if [ "$family" = "ipv4" ]; then
 		IPT="$IPT4"
 		IPTR="$IPT4R"
-	elif [ "$family" = "ipv6" ] && [ $NO_IPV6 -eq 0 ]; then
+	elif [ "$family" = "ipv6" ] && [ "$NO_IPV6" -eq 0 ]; then
 		IPT="$IPT6"
 		IPTR="$IPT6R"
 	else
@@ -427,7 +427,7 @@ mwan3_delete_iface_iptables()
 	fi
 
 	if [ "$family" = "ipv6" ]; then
-		[ $NO_IPV6 -ne 0 ] && return
+		[ "$NO_IPV6" -ne 0 ] && return
 		IPT="$IPT6"
 	fi
 
@@ -482,8 +482,7 @@ mwan3_create_iface_route()
 			# possible that routes are already in the table
 			# if 'connected' was called after 'ifup'
 			[ -n "$tbl" ] && [ -z "${tbl##*$route_line$'\n'*}" ] && continue
-			$IP route add table $id $route_line ||
-				LOG warn "failed to add $route_line to table $id"
+			$IP route add table $id $route_line || LOG debug "Route '$route_line' already added to table $id"
 		fi
 
 	done
@@ -503,7 +502,7 @@ mwan3_delete_iface_route()
 
 	if [ "$family" = "ipv4" ]; then
 		$IP4 route flush table "$id"
-	elif [ "$family" = "ipv6" ] && [ $NO_IPV6 -eq 0 ]; then
+	elif [ "$family" = "ipv6" ] && [ "$NO_IPV6" -eq 0 ]; then
 		$IP6 route flush table "$id"
 	fi
 }
@@ -519,7 +518,7 @@ mwan3_create_iface_rules()
 
 	if [ "$family" = "ipv4" ]; then
 		IP="$IP4"
-	elif [ "$family" = "ipv6" ] && [ $NO_IPV6 -eq 0 ]; then
+	elif [ "$family" = "ipv6" ] && [ "$NO_IPV6" -eq 0 ]; then
 		IP="$IP6"
 	else
 		return
@@ -543,7 +542,7 @@ mwan3_delete_iface_rules()
 
 	if [ "$family" = "ipv4" ]; then
 		IP="$IP4"
-	elif [ "$family" = "ipv6" ] && [ $NO_IPV6 -eq 0 ]; then
+	elif [ "$family" = "ipv6" ] && [ "$NO_IPV6" -eq 0 ]; then
 		IP="$IP6"
 	else
 		return
@@ -564,8 +563,7 @@ mwan3_delete_iface_ipset_entries()
 
 	for setname in $(ipset -n list | grep ^mwan3_rule_); do
 		for entry in $(ipset list "$setname" | grep "$(mwan3_id2mask id MMX_MASK | awk '{ printf "0x%08x", $1; }')" | cut -d ' ' -f 1); do
-			$IPS del "$setname" $entry ||
-				LOG notice "failed to delete $entry from $setname"
+			$IPS del "$setname" $entry || LOG notice "failed to delete $entry from $setname"
 		done
 	done
 }
@@ -582,7 +580,7 @@ mwan3_set_policy()
 
 	[ -n "$iface" ] || return 0
 	network_get_device device "$iface"
-	[ "$metric" -gt $DEFAULT_LOWEST_METRIC ] && LOG warn "Member interface $iface has >$DEFAULT_LOWEST_METRIC metric. Not appending to policy" && return 0
+	[ "$metric" -gt "$DEFAULT_LOWEST_METRIC" ] && LOG warn "Member interface $iface has >$DEFAULT_LOWEST_METRIC metric. Not appending to policy" && return 0
 
 	mwan3_get_iface_id id "$iface"
 
@@ -603,7 +601,7 @@ mwan3_set_policy()
 	current="$($IPT -S)"$'\n'
 	update="*mangle"
 
-	if [ "$family" = "ipv4" ] && [ $is_offline -eq 0 ]; then
+	if [ "$family" = "ipv4" ] && [ "$is_offline" -eq 0 ]; then
 		if [ "$metric" -lt "$lowest_metric_v4" ]; then
 			is_lowest=1
 			total_weight_v4=$weight
@@ -614,7 +612,7 @@ mwan3_set_policy()
 		else
 			return
 		fi
-	elif [ "$family" = "ipv6" ] && [ $NO_IPV6 -eq 0 ] && [ $is_offline -eq 0 ]; then
+	elif [ "$family" = "ipv6" ] && [ "$NO_IPV6" -eq 0 ] && [ "$is_offline" -eq 0 ]; then
 		if [ "$metric" -lt "$lowest_metric_v6" ]; then
 			is_lowest=1
 			total_weight_v6=$weight
@@ -626,19 +624,19 @@ mwan3_set_policy()
 			return
 		fi
 	fi
-	if [ $is_lowest -eq 1 ]; then
+	if [ "$is_lowest" -eq 1 ]; then
 		mwan3_push_update -F "mwan3_policy_$policy"
 		mwan3_push_update -A "mwan3_policy_$policy" \
 				  -m mark --mark 0x0/$MMX_MASK \
 				  -m comment --comment \"$iface $weight $weight\" \
 				  -j MARK --set-xmark "$(mwan3_id2mask id MMX_MASK)/$MMX_MASK"
-	elif [ $is_offline -eq 0 ]; then
+	elif [ "$is_offline" -eq 0 ]; then
 		probability=$((weight*1000/total_weight))
 		if [ "$probability" -lt 10 ]; then
 			probability="0.00$probability"
-		elif [ $probability -lt 100 ]; then
+		elif [ "$probability" -lt 100 ]; then
 			probability="0.0$probability"
-		elif [ $probability -lt 1000 ]; then
+		elif [ "$probability" -lt 1000 ]; then
 			probability="0.$probability"
 		else
 			probability="1"
@@ -652,7 +650,7 @@ mwan3_set_policy()
 				  -m comment --comment \"$iface $weight $total_weight\" \
 				  -j MARK --set-xmark "$(mwan3_id2mask id MMX_MASK)/$MMX_MASK"
 	elif [ -n "$device" ]; then
-		echo "$current" | grep -q "^-A mwan3_policy_$policy.*--comment .* [0-9]* [0-9]*" ||
+		echo "$current" | grep -q "^-A mwan3_policy_$policy.*--comment .* [0-9]* [0-9]*" || \
 			mwan3_push_update -I "mwan3_policy_$policy" \
 					  -o "$device" \
 					  -m mark --mark 0x0/$MMX_MASK \
@@ -679,7 +677,7 @@ mwan3_create_policies_iptables()
 	fi
 
 	for IPT in "$IPT4" "$IPT6"; do
-		[ "$IPT" = "$IPT6" ] && [ $NO_IPV6 -ne 0 ] && continue
+		[ "$IPT" = "$IPT6" ] && [ "$NO_IPV6" -ne 0 ] && continue
 		current="$($IPT -S)"$'\n'
 		update="*mangle"
 		if [ -n "${current##*-N mwan3_policy_$1$'\n'*}" ]; then
@@ -773,7 +771,7 @@ mwan3_set_sticky_ipset()
 		hash:ip,mark markmask "$mmx" \
 		timeout "$timeout"
 
-	[ $NO_IPV6 -eq 0 ] &&
+	[ "$NO_IPV6" -eq 0 ] &&
 		mwan3_push_update -! create "mwan3_rule_ipv6_$rule" \
 			hash:ip,mark markmask "$mmx" \
 			timeout "$timeout" family inet6
@@ -806,12 +804,12 @@ mwan3_set_user_iptables_rule()
 	config_get global_logging globals logging 0
 	config_get loglevel globals loglevel notice
 
-	[ "$ipv" = "ipv6" ] && [ $NO_IPV6 -ne 0 ] && return
+	[ "$ipv" = "ipv6" ] && [ "$NO_IPV6" -ne 0 ] && return
 	[ "$family" = "ipv4" ] && [ "$ipv" = "ipv6" ] && return
 	[ "$family" = "ipv6" ] && [ "$ipv" = "ipv4" ] && return
 
 	for ipaddr in "$src_ip" "$dest_ip"; do
-		if [ -n "$ipaddr" ] && { { [ "$ipv" = "ipv4" ] && echo "$ipaddr" | grep -qE "$IPv6_REGEX"; } ||
+		if [ -n "$ipaddr" ] && { { [ "$ipv" = "ipv4" ] && echo "$ipaddr" | grep -qE "$IPv6_REGEX"; } || \
 						 { [ "$ipv" = "ipv6" ] && echo "$ipaddr" | grep -qE $IPv4_REGEX; } }; then
 			LOG warn "invalid $ipv address $ipaddr specified for rule $rule"
 			return
@@ -869,17 +867,17 @@ mwan3_set_user_iptables_rule()
 		fi
 	fi
 
-	if [ $rule_policy -eq 1 ] && [ -n "${current##*-N $policy$'\n'*}" ]; then
+	if [ "$rule_policy" -eq 1 ] && [ -n "${current##*-N $policy$'\n'*}" ]; then
 		mwan3_push_update -N "$policy"
 	fi
 
-	if [ $rule_policy -eq 1 ] && [ "$sticky" -eq 1 ]; then
+	if [ "$rule_policy" -eq 1 ] && [ "$sticky" -eq 1 ]; then
 		if [ -n "${current##*-N mwan3_rule_$1$'\n'*}" ]; then
 			mwan3_push_update -N "mwan3_rule_$1"
 		fi
 
 		mwan3_push_update -F "mwan3_rule_$1"
-		config_foreach mwan3_set_sticky_iptables interface $ipv "$policy"
+		config_foreach mwan3_set_sticky_iptables interface "$rule" "$ipv" "$policy"
 
 
 		mwan3_push_update -A "mwan3_rule_$1" \
@@ -951,7 +949,7 @@ mwan3_set_user_iface_rules()
 		[ "$src_iface" = "$iface" ] && is_src_iface=1
 	}
 	config_foreach iface_rule rule
-	[ $is_src_iface -eq 1 ] && mwan3_set_user_rules
+	[ "$is_src_iface" -eq 1 ] && mwan3_set_user_rules
 }
 
 mwan3_set_user_rules()
@@ -967,7 +965,7 @@ mwan3_set_user_rules()
 			IPT="$IPT6"
 			IPTR="$IPT6R"
 		fi
-		[ "$ipv" = "ipv6" ] && [ $NO_IPV6 -ne 0 ] && continue
+		[ "$ipv" = "ipv6" ] && [ "$NO_IPV6" -ne 0 ] && continue
 		update="*mangle"
 		current="$($IPT -S)"$'\n'
 
@@ -1117,13 +1115,13 @@ mwan3_report_iface_status()
 
 	if [ "$result" = "offline" ]; then
 		:
-	elif [ $error -eq 0 ]; then
+	elif [ "$error" -eq 0 ]; then
 		online=$(get_online_time "$1")
 		network_get_uptime uptime "$1"
 		online="$(printf '%02dh:%02dm:%02ds\n' $((online/3600)) $((online%3600/60)) $((online%60)))"
 		uptime="$(printf '%02dh:%02dm:%02ds\n' $((uptime/3600)) $((uptime%3600/60)) $((uptime%60)))"
 		result="$(mwan3_get_iface_hotplug_state $1) $online, uptime $uptime"
-	elif [ $error -gt 0 ] && [ $error -ne 31 ]; then
+	elif [ "$error" -gt 0 ] && [ "$error" -ne 31 ]; then
 		result="error (${error})"
 	elif [ "$enabled" = "1" ]; then
 		result="offline"
@@ -1142,11 +1140,11 @@ mwan3_report_policies()
 
 	local percent total_weight weight iface
 
-	total_weight=$($ipt -S "$policy" | grep -v '.*--comment "out .*" .*$' | cut -s -d'"' -f2 | head -1 | awk '{print $3}')
+	total_weight=$($ipt -S "$policy" | grep -v '.*--comment "out .*" .*$' | cut -s -d'"' -f2 2>/dev/null | head -1 | awk '{print $3}')
 
 	if [ -n "${total_weight##*[!0-9]*}" ]; then
-		for iface in $($ipt -S "$policy" | grep -v '.*--comment "out .*" .*$' | cut -s -d'"' -f2 | awk '{print $1}'); do
-			weight=$($ipt -S "$policy" | grep -v '.*--comment "out .*" .*$' | cut -s -d'"' -f2 | awk '$1 == "'$iface'"' | awk '{print $2}')
+		for iface in $($ipt -S "$policy" | grep -v '.*--comment "out .*" .*$' | cut -s -d'"' -f2 2>/dev/null | awk '{print $1}'); do
+			weight=$($ipt -S "$policy" | grep -v '.*--comment "out .*" .*$' | cut -s -d'"' -f2 2>/dev/null | awk '$1 == "'$iface'"' | awk '{print $2}')
 			percent=$((weight*100/total_weight))
 			echo " $iface ($percent%)"
 		done
