2f0d4ad735ab7b0121f09b8bcb53e0bf6f5fba21
loso3000 <51484662+loso3000@users.noreply.github.com>
2023-10-26 13:46:46 +0800

    z

--- common/tools.sh
+++ common/tools.sh
@@ -9,7 +9,7 @@
 #=================================================
 
 mkdir -p files/root
-cp  -rf ./patch/z.zshrc ./files/root/.zshrc
+cp  -rf ./package/other/patch/z.zshrc ./files/root/.zshrc
 pushd files/root
 
 ## Install oh-my-zsh
