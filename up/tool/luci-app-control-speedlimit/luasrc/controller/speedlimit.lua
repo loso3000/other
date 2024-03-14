8b62f68ab8f3340ae73cc89e3940c40e4f9e3ebd
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2024-03-14 23:27:30 +0800

    x

--- up/tool/luci-app-control-speedlimit/luasrc/controller/speedlimit.lua
+++ up/tool/luci-app-control-speedlimit/luasrc/controller/speedlimit.lua
@@ -3,7 +3,7 @@ module("luci.controller.speedlimit", package.seeall)
 function index()
 	if not nixio.fs.access("/etc/config/speedlimit") then return end
         entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
-	local e = entry({"admin","control","speedlimit"},cbi("speedlimit"),_("SpeedLimit"),67)
+	local e = entry({"admin","control","speedlimit"},cbi("speedlimit"),_("Speed Limit"),67)
 	e.dependent=false
         e.acl_depends = { "luci-app-speedlimit" }
  end
