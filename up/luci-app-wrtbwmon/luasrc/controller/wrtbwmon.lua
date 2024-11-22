42538e0a7b3abed87c8915df804643d54a0a2e77
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2024-11-22 23:15:29 +0800

    x

--- up/luci-app-wrtbwmon/luasrc/controller/wrtbwmon.lua
+++ up/luci-app-wrtbwmon/luasrc/controller/wrtbwmon.lua
@@ -6,7 +6,7 @@ function index()
 	end
 	entry({"admin", "status", "usage"},alias("admin", "status", "usage", "details"), _("Traffic statistics"), 60)
 	entry({"admin", "status", "usage", "details"}, template("wrtbwmon"),_("Details"), 10).leaf=true
-	entry({"admin", "status", "usage", "details"}, template("onlinuser"),_("Online User""), 12).leaf=true
+	entry({"admin", "status", "usage", "onlinuser"}, template("onlinuser"),_("Online User""), 12).leaf=true
 	entry({"admin", "status", "usage", "config"},arcombine(cbi("wrtbwmon/config")),_("Configuration"), 20).leaf=true
 	entry({"admin", "status", "usage", "custom"},form("wrtbwmon/custom"),_("User file"), 30).leaf=true
 
