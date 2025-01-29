b4547ef8d3c2429f4199256aab5e3aaade88046b
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-01-29 14:56:29 +0800

    x

--- common/tools.sh
+++ common/tools.sh
@@ -8,32 +8,8 @@
 # Blog: https://mlapp.cn
 #=================================================
 
-if [ $1 == xxamd64 ] ;then
-BASE_FILES=${GITHUB_WORKSPACE}/openwrt/package/base-files/files
-singbox_version="1.8.8"
-hysteria_version="2.3.0"
-wget --quiet --no-check-certificate -P /tmp https://github.com/SagerNet/sing-box/releases/download/v${singbox_version}/sing-box-${singbox_version}-linux-amd64.tar.gz
-wget --quiet --no-check-certificate -P /tmp https://github.com/apernet/hysteria/releases/download/app%2Fv${hysteria_version}/hysteria-linux-amd64
-mkdir  -p ${BASE_FILES}/usr/bin
-tar -xvzf /tmp/sing-box-${singbox_version}-linux-amd64.tar.gz -C /tmp
-cp -r /tmp/sing-box-${singbox_version}-linux-amd64/sing-box ${BASE_FILES}/usr/bin
-cp -r /tmp/hysteria-linux-amd64 ${BASE_FILES}/usr/bin/hysteria
-chmod 777 ${BASE_FILES}/usr/bin/sing-box ${BASE_FILES}/usr/bin/hysteria
-fi
-#删除冲突插件
-# rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\).*")
-#修改默认主题
-# sed -i "s/luci-theme-bootstrap/luci-theme-$OWRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
-#修改默认IP地址
-# sed -i "s/192\.168\.[0-9]*\.[0-9]*/$OWRT_IP/g" ./package/base-files/files/bin/config_generate
-#修改默认主机名
-# sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/config_generate
-#修改默认时区
-sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
-sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
 mkdir -p files/root
-cp  -rf ./package/other/patch/z.zshrc ./files/root/.zshrc
-cp  -rf ./package/other/patch/profiles ./files/etc/profiles
+cp  -rf ./patch/z.zshrc ./files/root/.zshrc
 pushd files/root
 
 ## Install oh-my-zsh
@@ -45,4 +21,22 @@ git clone https://github.com/ohmyzsh/ohmyzsh ./.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+popd
+
+## opkg ##
+PLATFORM=$(cat .config | grep CONFIG_TARGET_ARCH_PACKAGES | awk -F '"' '{print $2}')
+TARGET=$(cat .config | grep CONFIG_TARGET_BOARD | awk -F '"' '{print $2}')
+SUBTARGET=$(cat .config |  grep CONFIG_TARGET | sed -n 2p | awk -F '=' '{print $1}')
+
+mkdir -p files/etc/opkg
+pushd files/etc/opkg
+cat <<-EOF > "distfeeds.conf"
+src/gz ezopwrt_core https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/targets/x86/64/packages
+src/gz ezopwrt_base https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/base
+src/gz ezopwrt_luci https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/luci
+src/gz ezopwrt_packages https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/packages
+src/gz ezopwrt_routing https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/routing
+src/gz ezopwrt_telephony https://downloads.immortalwrt.org/releases/23.05-SNAPSHOT/packages/x86_64/telephony
+EOF
+cp distfeeds.conf distfeeds.conf.bak
 popd
