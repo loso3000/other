12e0ed529cffe3bde8fe16be6cbf5930e00aa6c8
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-03-04 00:19:31 +0800

    x

--- up/tool/luci-app-kucat-config/htdocs/luci-static/resources/view/kucat-config.js
+++ up/tool/luci-app-kucat-config/htdocs/luci-static/resources/view/kucat-config.js
@@ -1,14 +1,18 @@
 'use strict';
 'require form';
+'require fs';
 'require rpc';
 'require uci';
 'require ui';
 'require view';
+var callAvailSpace = rpc.declare({
+	object: 'luci.kucat',
+	method: 'avail'
+});
 
-var ts_sets = [0, 0.1, 0.2, 0.3, 0.4,
-	0.5, 0.6, 0.7, 0.8, 0.9, 1 ];
-var opacity_sets = [0, 1, 2, 3, 4,
-	5, 6, 7, 8, 9, 10, 20, 30, 50, 100 ];
+var bg_path = '/www/luci-static/kucat/bg/';
+var ts_sets = [0, 0.1, 0.2, 0.3, 0.4,0.5, 0.6, 0.7, 0.8, 0.9, 1 ];
+var opacity_sets = [0, 1, 2, 3, 4,5, 6, 7, 8, 9, 10, 20, 30, 50, 100 ];
 
 function createColorPicker(textInput) {
 	const colorPicker = document.createElement('input');
@@ -32,7 +36,9 @@ function createColorPicker(textInput) {
 return view.extend({
 	load: function() {
 		return Promise.all([
-			uci.load('kucat')
+			uci.load('kucat'),
+			L.resolveDefault(callAvailSpace(), {}),
+			L.resolveDefault(fs.list(bg_path), {})
 		]);
 	},
 
@@ -42,7 +48,10 @@ return view.extend({
 		m = new form.Map('kucat', _('KuCat Theme Config'),
 			_('Set and manage features such as KuCat theme background wallpaper, main background color, partition background, transparency, blur, toolbar retraction and shortcut pointing.'));
 
-		s = m.section(form.TypedSection, 'basic', _('Theme Config'));
+		s = m.section(form.TypedSection, 'basic');
+		s.addremove = false;
+		s.anonymous = true;
+
 		o = s.option(form.ListValue, 'background', _('Wallpaper Source'));
 		o.value('0', _('Local wallpaper'));
 		o.value('1', _('Auto download Iciba wallpaper'));
@@ -53,11 +62,9 @@ return view.extend({
 		o.rmempty = false;
 
 		o = s.option(form.Flag, 'bklock', _('Wallpaper synchronization'));
-		o.default = o.disabled;
 		o.rmempty = false;
 
 		o = s.option(form.Flag, 'setbar', _('Expand Toolbar'));
-		o.default = o.disabled;
 		o.rmempty = false;
 
 		o = s.option(form.Flag, 'bgqs', _('Refreshing mode'));
@@ -65,7 +72,6 @@ return view.extend({
 		o.rmempty = false;
 
 		o = s.option(form.Flag, 'dayword', _('Enable Daily Word'));
-		o.default = o.disabled;
 		o.rmempty = false;
 
 		o = s.option(form.ListValue, 'gohome', _('Status Homekey settings'));
@@ -97,35 +103,35 @@ return view.extend({
 		o.rmempty = false;
 		
 		o = s.option(form.Flag, 'fontmode', _('Care mode (large font)'));
-		o.default = o.disabled;
 		o.rmempty = false;
 
-		o = s.option(form.Value, 'primary', _('[Light mode] Primary Color'), _('A HEX color (default: #5e72e4).'))
-		o.default = '#5e72e4';
+		o = s.option(form.Value, 'progresscolor', _('Progress bar Font Color'))
+		o.default = '#146db3e6';
 		o.rmempty = false;
-		o.validate = function(section_id, value) {
-			if (section_id)
-				return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(value) ||
-					_('Expecting: %s').format(_('valid HEX color value'));
-			return true;
-		};
+
 		o.render = function(section_id, option_index, cfgvalue) {
 			var el = form.Value.prototype.render.apply(this, arguments);
 			setTimeout(function() {
-				const textInput = document.querySelector('[id^='widget.cbid.kucat.'][id$='.primary']');
+				const textInput = document.querySelector('[id^="widget.cbid.kucat."][id$=".progresscolor"]');
 				createColorPicker(textInput);
 			}, 0);
 			return el;
 		};
 
+		o = s.option(form.Button, '_save', _('Save settings'));
+		o.inputstyle = 'apply';
+		o.inputtitle = _('Save current settings');
+		o.onclick = function() {
+			ui.changes.apply(true);
+			return this.map.save(null, true);
+		}
+
 		s = m.section(form.TableSection, 'theme', _('Color scheme list'));
 		s.addremove = true;
 		s.anonymous = true;
-		s.sortable = true;
 
 		o = s.option(form.Value, 'remarks', _('Remarks'));
 		o = s.option(form.Flag, 'use', _('Enable color matching'));
-
 		o.default = o.disabled;
 		o.rmempty = false;
 
@@ -137,13 +143,13 @@ return view.extend({
 		o.rmempty = false;
 
 		o = s.option(form.Value, 'primary_rgbm', _('Main Background color(RGB)'));
-		o:value('blue',_('RoyalBlue'));
-		o:value('green',_('MediumSeaGreen'));
-		o:value('orange',_('SandyBrown'));
-		o:value('red',_('TomatoRed'));
-		o:value('black',_('Black tea eye protection gray'));
-		o:value('gray',_('Cool night time(gray and dark)'));
-		o:value('bluets',_('Cool Ocean Heart (transparent and bright)'));
+		o.value('blue',_('RoyalBlue'));
+		o.value('green',_('MediumSeaGreen'));
+		o.value('orange',_('SandyBrown'));
+		o.value('red',_('TomatoRed'));
+		o.value('black',_('Black tea eye protection gray'));
+		o.value('gray',_('Cool night time(gray and dark)'));
+		o.value('bluets',_('Cool Ocean Heart (transparent and bright)'));
 		o.default = 'blue';
 		o.rmempty = false;
 		
@@ -151,7 +157,7 @@ return view.extend({
 		o.default = o.disabled;
 		o.rmempty = false;
 	
-		o = s.option(form.Value, 'primary_rgbm', _('Wallpaper transparency'));
+		o = s.option(form.Value, 'primary_rgbm_ts', _('Wallpaper transparency'));
 		for (var i of ts_sets)
 			o.value(i);
 		o.default = '0.9';
@@ -165,7 +171,6 @@ return view.extend({
 
 		o = s.option(form.Value, 'primary_rgbs', _('Fence background(RGB)'));
 		o.default = '225,112,88';
-		o.datatype = ufloat;
 		o.rmempty = false;
 
 		o = s.option(form.Value, 'primary_rgbs_ts', _('Fence background transparency'));
@@ -174,7 +179,11 @@ return view.extend({
 		o.default = '0.1';
 		o.rmempty = false;
 
-		return m.render();
-	}
 
+		return m.render();
+	},
+
+	handleSaveApply: null,
+	handleSave: null,
+	handleReset: null
 });
