b24616c4bbc72e818a0df2ad5b23071895562cb1
loso3000 sir <51484662+loso3000@users.noreply.github.com>
2025-04-10 09:52:55 +0800

    u

--- up/tool/autocore/files/generic/19_cpu.js
+++ up/tool/autocore/files/generic/19_cpu.js
@@ -2,67 +2,224 @@
 'require baseclass';
 'require fs';
 
+document.head.append(E('style', {'type': 'text/css'},
+`
+.cpu-status-view-mode-entry {
+	display: inline-block;
+	cursor: pointer;
+	margin: 2px !important;
+	padding: 2px 4px;
+	border: 1px dotted;
+	-webkit-border-radius: 4px;
+	-moz-border-radius: 4px;
+	border-radius: 4px;
+	opacity: 0.7;
+}
+.cpu-status-view-mode-entry-checked {
+	border: 1px solid;
+	opacity: 1;
+}
+`));
+
 return baseclass.extend({
-	title    : _('CPU usage'),
+	title             : _('CPU usage'),
 
-	statArray: null,
+	viewName          : 'cpu_status',
 
-	load     : function() {
-		return L.resolveDefault(fs.read('/proc/stat'), null);
+	availableViewModes: {
+		0: [ 'allCPUs', _('Load') ],
+		1: [ 'eachCPU', _('Detailed load') ],
 	},
 
-	render: function(cpuData) {
-		if(!cpuData) return;
+	viewMode          : 0,
 
+	lastStatArray     : null,
+
+	restoreSettingsFromLocalStorage() {
+		let viewModeLocal = localStorage.getItem(`luci-app-${this.viewName}-viewMode`);
+		if(viewModeLocal) {
+			this.viewMode = Number(viewModeLocal);
+		};
+	},
+
+	saveSettingsToLocalStorage(viewMode) {
+		if(this.viewMode != viewMode) {
+			localStorage.setItem(
+				`luci-app-${this.viewName}-viewMode`, String(viewMode));
+		};
+	},
+
+	parseProcData(data) {
 		let cpuStatArray   = [];
-		let statItemsArray = cpuData.trim().split('\n').filter(s => s.startsWith('cpu'));
+		let statItemsArray = data.trim().split('\n').filter(s => s.startsWith('cpu'));
 
 		for(let str of statItemsArray) {
 			let arr = str.split(/\s+/).slice(0, 8);
 			arr[0]  = (arr[0] === 'cpu') ? Infinity : arr[0].replace('cpu', '');
-			arr     = arr.map(e => Number(e));
-			cpuStatArray.push([
-				arr[0],
-				arr[1] + arr[2] + arr[3] + arr[5] + arr[6] + arr[7],
-				arr[4],
-			]);
+			cpuStatArray.push(arr.map(e => Number(e)));
 		};
 
 		cpuStatArray.sort((a, b) => a[0] - b[0]);
+		return cpuStatArray;
+	},
 
-		let cpuTable = E('table', { 'class': 'table' });
-
-		// For single-core CPU (hide 'total')
-		if(cpuStatArray.length === 2) {
-			cpuStatArray = cpuStatArray.slice(0, 1);
-		};
-
+	calcCPULoad(cpuStatArray) {
+		let retArray = [];
 		cpuStatArray.forEach((c, i) => {
-			let loadAvg = 0;
-			if(this.statArray !== null) {
-				let idle = c[2] - this.statArray[i][2];
-				let sum  = c[1] - this.statArray[i][1];
-				loadAvg  = Math.round(100 * sum / (sum + idle));
+			let loadUser = 0,
+			    loadNice = 0,
+			    loadSys  = 0,
+			    loadIdle = 0,
+			    loadIo   = 0,
+			    loadIrq  = 0,
+			    loadSirq = 0,
+			    loadAvg  = 0;
+			if(this.lastStatArray !== null && this.lastStatArray.cpuNum === c.cpuNum) {
+				let user = c[1] - this.lastStatArray[i][1],
+				    nice = c[2] - this.lastStatArray[i][2],
+				    sys  = c[3] - this.lastStatArray[i][3],
+				    idle = c[4] - this.lastStatArray[i][4],
+				    io   = c[5] - this.lastStatArray[i][5],
+				    irq  = c[6] - this.lastStatArray[i][6],
+				    sirq = c[7] - this.lastStatArray[i][7];
+				let sum  = user + nice + sys + idle + io + irq + sirq;
+				loadUser = Number((100 * user / sum).toFixed(1));
+				loadNice = Number((100 * nice / sum).toFixed(1));
+				loadSys  = Number((100 * sys / sum).toFixed(1));
+				loadIdle = Number((100 * idle / sum).toFixed(1));
+				loadIo   = Number((100 * io / sum).toFixed(1));
+				loadIrq  = Number((100 * irq / sum).toFixed(1));
+				loadSirq = Number((100 * sirq / sum).toFixed(1));
+				loadAvg  = Math.round(100 * (user + nice + sys + io + irq + sirq) / sum);
 			};
+			retArray.push(
+				{cpuNum: c[0], loadUser, loadNice, loadSys, loadIdle, loadIo, loadIrq, loadSirq, loadAvg});
+		});
+		return retArray;
+	},
 
-			cpuTable.append(
+	allCPUs: {
+		makeTable() {
+			this.table = E('table', { 'class': 'table' });
+		},
+		append(cpuNum, cpuLoadObj, cpuLoadFlag) {
+			if(cpuNum === Infinity) {
+				this.table.append(
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'width': '33%' },
+							_('Total usage')),
+						E('td', { 'class': 'td' },
+							E('div', {
+									'class': 'cbi-progressbar',
+									'title': (cpuLoadFlag) ?
+										cpuLoadObj.loadAvg + '%' :
+										_('Calculating') + '...',
+								},
+								E('div', { 'style': 'width:' + cpuLoadObj.loadAvg + '%' })
+							)
+						),
+					])
+				);
+			};
+		},
+	},
+
+	eachCPU: {
+		makeTable() {
+			this.table = E('table', { 'class': 'table' });
+		},
+		append(cpuNum, cpuLoadObj, cpuLoadFlag) {
+			this.table.append(
 				E('tr', { 'class': 'tr' }, [
-					E('td', { 'class': 'td right', 'width': '33%' },
-						(cpuStatArray[i][0] === Infinity) ? _('Total usage') : _('CPU') + ' ' + cpuStatArray[i][0]),
-
+					E('td', { 'class': 'td left', 'width': '33%' },
+						(cpuNum === Infinity) ? _('Total usage') : _('CPU') + ' ' + cpuNum),
 					E('td', { 'class': 'td' },
 						E('div', {
 								'class': 'cbi-progressbar',
-								'title': (this.statArray !== null) ? loadAvg + '%' : _('Calculating') + '...',
+								'title': (cpuLoadFlag) ?
+									cpuLoadObj.loadAvg + '%' :
+									_('Calculating') + '...',
 							},
-							E('div', { 'style': 'width:' + loadAvg + '%' })
+							E('div', { 'style': 'width:' + cpuLoadObj.loadAvg + '%' })
 						)
 					),
 				])
 			);
-		});
+		},
+	},
 
-		this.statArray = cpuStatArray;
-		return cpuTable;
+	makeCPUTable(cpuLoadArray) {
+		let currentView = (this.availableViewModes[this.viewMode] !== undefined) ?
+			this[this.availableViewModes[this.viewMode][0]] :
+			this[this.availableViewModes[0][0]];
+		currentView.makeTable();
+		cpuLoadArray.forEach((c, i) => {
+			currentView.append(c.cpuNum, c, (this.lastStatArray !== null));
+		});
+		return currentView.table;
+	},
+
+	load() {
+		this.restoreSettingsFromLocalStorage();
+		return L.resolveDefault(fs.read('/proc/stat'), null);
+	},
+
+	render(cpuData) {
+		if(!cpuData) return;
+
+		let cpuStatArray = this.parseProcData(cpuData);
+
+		// For single-core CPU
+		if(cpuStatArray.length === 2) {
+			cpuStatArray = cpuStatArray.slice(1, 2);
+			delete this.availableViewModes[2];
+			delete this.availableViewModes[3];
+			if(this.viewMode > 1) this.viewMode = 0;
+		};
+
+		let cpuLoadArray = this.calcCPULoad(cpuStatArray);
+		let cpuTable = this.makeCPUTable(cpuLoadArray);
+
+		let viewModeSelectFunc = value => {
+			this.saveSettingsToLocalStorage(value);
+			this.viewMode = value;
+			viewModeEntries.forEach(i => {
+				i.classList.remove('cpu-status-view-mode-entry-checked');
+			});
+			viewModeEntries[value].classList.add('cpu-status-view-mode-entry-checked');
+			let newTable = this.makeCPUTable(cpuLoadArray);
+			cpuTable.replaceWith(newTable);
+			cpuTable = newTable;
+		};
+
+		let viewModeSelectOnClick = ev => {
+			viewModeSelectFunc(ev.target.dataset.value);
+			ev.target.blur();
+		};
+
+		let viewModeEntries = [];
+
+		for(let [i, v] of Object.entries(this.availableViewModes)) {
+			viewModeEntries.push(
+				E('span', {
+					'class'     : 'cpu-status-view-mode-entry',
+					'href'      : 'javascript:void(0)',
+					'data-value': i,
+					'click'     : viewModeSelectOnClick,
+				}, v[1])
+			);
+		};
+
+		viewModeSelectFunc(this.viewMode);
+
+		this.lastStatArray = cpuStatArray;
+
+		return E('div', { 'class': 'cbi-section' }, [
+			E('div',
+				{ 'style': 'margin-bottom:1em; padding:0 4px;' },
+				viewModeEntries
+			),
+			cpuTable,
+		]);
 	},
 });
